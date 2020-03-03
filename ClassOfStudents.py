980ef17e2afe7b9a9969e67ab5668d64126cdcf4
Catherine1000 <cathwilson00@gmail.com>
2020-03-03 14:29:34 +0000

    Tuesday Exercises

---
+++ ClassOfStudents.py
@@ -0,0 +1,13 @@
+class students:
+    def __init__(self, name, age):
+
+        self.name = name
+        self.age = age
+
+    class_ = "student"
+
+    def average(maths, science, english):
+        maths = ()
+
+        score = (maths * science * english) / 3
+        return average
---
+++ Classes.py
@@ -0,0 +1,11 @@
+class student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+
+John = student("John", "21")
+Jane = student("Jane", "22")
+
+print(getattr(John, "age"))
